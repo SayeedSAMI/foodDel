f38bced5398de686b8e5f8bdc01300982ac01c81
SayeedSami <sayeedsami26@gmail.com>
2024-12-25 12:11:04 +0600

    first commit

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Navbar.css'
+
+const navbar = () => {
+  return (
+    <div className='navbar'>
+  <img src={} alt="" />
+    </div>
+  )
+}
+
+export default navbar
