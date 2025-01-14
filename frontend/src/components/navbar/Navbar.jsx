1952040df10e3611d29d860dc785f1ae718861bd
SayeedSami <sayeedsami26@gmail.com>
2025-01-15 01:44:26 +0600

    full website done

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
 const navbar = ({ setShowLogin }) => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [menu, setMenu] = useState("home")
-
+  const [menu, setMenu] = useState("menu")
+  const { getTotalCartAmount } = useContext("StoreContext")
   return (
     <div className='navbar'>
       <Link to='/'> <img src={assets.logo} alt="" className="logo" /></Link>
@@ -22,11 +22,11 @@ const navbar = ({ setShowLogin }) => {
         <div className="navbar-search-icon">
           <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
 
-          <div className="dot"></div>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
         <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
-    </div>
+    </div >
   )
 }
 
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,11 +1,51 @@
-import  'react'
+import 'react'
 import './PlaceOrder.css'
 const PlaceOrder = () => {
+  const { getTotalCartAmount } = useContext(StoreContext)
   return (
-    <div>
-      
-    </div>
+    <form action="" className='Place-order'>
+      <div className="place-order-left">
+        <p className='title'>Delivery INformation</p>
+        <div className="multi-field">
+          <input type="text" placeholder='Frist name' />
+          <input type="text" placeholder='Last name' />
+        </div>
+        <input type="email" placeholder='Email address' />
+        <input type="text" placeholder='Street' />
+      </div>
+      <div className="multi-field">
+        <input type="text" placeholder='City' />
+        <input type="text" placeholder='State' />
+      </div>
+      <div className="multi-field">
+        <input type="text" placeholder='Zip code' />
+        <input type="text" placeholder='country' />
+      </div>
+      <input type="text" placeholder='Phone' />
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <div className="cart-total-details">
+              <p>Total</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</p>
+            </div>
+          </div>
+          <button>PROCEED TO Payment</button>
+        </div>
+      </div>
+    </form>
   )
 }
 import './PlaceOrder.css'
+import { useContext } from 'react'
+import { StoreContext } from '../../context/StoreContext'
 export default PlaceOrder
