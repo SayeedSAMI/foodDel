68994ccdd78055b5d199acb8085c8986f0de8d49
SayeedSami <sayeedsami26@gmail.com>
2025-01-14 23:34:06 +0600

    cart added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/navbar/navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
+import LoginPopUP from "./components/LoginPopUp/LoginPopUP";
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false)
   return (
     <>
+      {showLogin ? <LoginPopUP /> : <></>}
+
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
